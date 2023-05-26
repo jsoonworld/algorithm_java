24d174af5b9f83686cd33598023cf49bd1c886f3
권장순 <jsoonworld@gmail.com>
2023-05-26 14:47:06 +0900

    init

---
+++ src/main/java/com/example/algorithm_java/AlgorithmJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithm_java/AlgorithmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithm_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
